acd6cf1054f7100c872f3534e9677f2f4310b64a
artn <azemarteixeira@gmail.com>
2022-07-23 15:30:40 -0300

    Adding project to folder

---
+++ artn/nivel3/Timer/BCDCounter_mod6.v
@@ -0,0 +1,28 @@
+module BCDCounter_mod6 (
+  input  wire clrn, loadn, en, clk,
+  input  wire [3:0] data,
+  output reg  [3:0] out,
+  output reg  tc, zero
+);
+  reg [3:0] curr_state;
+
+  always @(posedge clk, negedge loadn, negedge clrn) begin
+    if (~clrn) begin
+      curr_state <= 0;
+    end
+    else if (~loadn) begin
+      curr_state <= data;
+    end
+    else if (en) begin
+      curr_state <= (curr_state == 0) ? 5 : curr_state - 1;
+    end
+
+    zero = (curr_state == 0) ? 1 : 0;
+    out = curr_state;
+  end
+
+  always @(posedge clk, posedge en) begin
+    tc = (en && curr_state == 0);
+  end
+
+endmodule
